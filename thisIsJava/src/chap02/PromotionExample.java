d7520b0c453ab23be0f5640032b12726545f071e
Rattanlegs <Rattanlegs@gmail.com>
2024-09-27 23:52:59 +0900

    흐음

--- thisIsJava/src/chap02/PromotionExample.java
+++ thisIsJava/src/chap02/PromotionExample.java
@@ -5,10 +5,23 @@ public class PromotionExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		byte byteValue = 10;
-		int intValue = byteValue;
+		int intValue = byteValue; //byte > int
 		
 		System.out.println(intValue);
 
+		char charValue = '가';
+		intValue = charValue; //char > int
+		System.out.println("가의 유니코드"+ intValue);
+
+		intValue = 500;
+		long longValue = intValue;
+		System.out.println(longValue);
+	
+		intValue = 200;
+		double doubleValue = intValue;
+		System.out.println(doubleValue);
+		
 	}
+	
 
 }
