f33ba91c40d2ede87d025e32b851206460adfcd0
Rattanlegs <Rattanlegs@gmail.com>
2025-03-12 23:56:51 +0900

    2/10 일

---
+++ thisIsJava/practice/src/chap02/PromotionExample.java
@@ -0,0 +1,14 @@
+package chap02;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		byte byteValue = 10;
+		int intValue = byteValue; // int < byte
+		
+		System.out.println(intValue);
+		
+		
+	}
+
+}
